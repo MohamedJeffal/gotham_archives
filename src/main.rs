4499f2b96036ae8097ec10c3d9dff723074f979a
Mohamed Jeffal <mh.jeffal@gmail.com>
2018-05-03 01:27:15 +0200

    feat(posts): Querying post list async + parsing with serde.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate futures;
 extern crate gotham;
 extern crate hyper;
 extern crate mime;
@@ -6,22 +7,71 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
+extern crate tokio_core;
+
+use futures::{Future, Stream};
 
 use hyper::{Response, StatusCode};
+use hyper::{Client, Uri};
 
 use gotham::http::response::create_response;
 use gotham::router::Router;
 use gotham::router::builder::*;
-use gotham::state::State;
-use gotham::handler::IntoResponse;
+use gotham::state::{FromState, State};
+use gotham::handler::{HandlerFuture, IntoHandlerError, IntoResponse};
 
-// https://jsonplaceholder.typicode.com/posts/1
+use tokio_core::reactor::Handle;
+
+type ResponseContentFuture = Box<Future<Item = Vec<Post>, Error = hyper::Error>>;
+
+fn http_get(handle: &Handle) -> ResponseContentFuture {
+    let client = Client::new(handle);
+    let url: Uri = "http://jsonplaceholder.typicode.com/posts".parse().unwrap();
+
+    let f = client.get(url).and_then(|response| {
+        response.body().concat2().and_then(
+            |full_body| Ok(serde_json::from_slice(&full_body).expect("expected serialized post")),
+        )
+    });
+
+    Box::new(f)
+}
+
+fn get_posts_handler(state: State) -> Box<HandlerFuture> {
+    let data_future: ResponseContentFuture = {
+        let handle = Handle::borrow_from(&state).clone();
+
+        let f = http_get(&handle);
+
+        Box::new(f)
+    };
+
+    Box::new(data_future.then(move |result| match result {
+        Ok(data) => {
+            let res = create_response(
+                &state,
+                StatusCode::Ok,
+                Some((
+                    serde_json::to_string(&data)
+                        .expect("expected serialized post")
+                        .into_bytes(),
+                    mime::APPLICATION_JSON,
+                )),
+            );
+
+            Ok((state, res))
+        }
+        Err(err) => Err((state, err.into_handler_error())),
+    }))
+}
+
 #[derive(Serialize, Deserialize)]
 struct Post {
+    #[serde(rename = "userId")]
     user_id: u8,
     id: u8,
     title: String,
-    body: String
+    body: String,
 }
 
 impl IntoResponse for Post {
@@ -33,18 +83,18 @@ impl IntoResponse for Post {
                 serde_json::to_string(&self)
                     .expect("expected serialized post")
                     .into_bytes(),
-                mime::APPLICATION_JSON
-            ))
+                mime::APPLICATION_JSON,
+            )),
         )
     }
 }
 
-fn get_post_handler(state: State) -> (State, Post) {
+fn sync_get_post_handler(state: State) -> (State, Post) {
     let post = Post {
         user_id: 12,
         id: 19,
         title: "Some post".to_string(),
-        body: "This is my first post, qskdsqdksqdshd".to_string()
+        body: "This is my first post, qskdsqdksqdshd".to_string(),
     };
 
     (state, post)
@@ -52,7 +102,8 @@ fn get_post_handler(state: State) -> (State, Post) {
 
 fn router() -> Router {
     build_simple_router(|route| {
-        route.get("/post").to(get_post_handler)
+        route.get("/posts").to(get_posts_handler);
+        route.get("/syncpost").to(sync_get_post_handler)
     })
 }
 
